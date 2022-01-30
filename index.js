801c08c5ea5807e2e660b45fa6ecbd6bfbc431af
Danan Ramadhan <danan@gramedia.id>
2022-01-31 00:29:58 +0700

    push new code

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ app.use(
   )
 
 app.get('/movies', db.getMovies)
-app.post('/movies/add', db.addMovies)
-app.post('/festivals/add', db.addFestivals)
-app.get('/festivals', db.getFestivals)
 app.get('/movies/:id', db.getDetailMovie)
+app.post('/movies/add', db.addMovies)
+app.delete('/movies/delete/:id', db.deleteMovie)
+
+app.post('/festivals/add', db.addFestivals)
+app.get('/festivals/:id', db.getDetailFestival)
+app.get('/festivals', db.getFestivals)
+app.delete('/festivals/delete/:id', db.deleteFestival)
+app.get('/asa/search/', db.searchFestival)
--- queries.js
+++ queries.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 const getMovies = (request, response) => {
   pool.query('SELECT * FROM movies ORDER BY id ASC', (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
     response.status(200).json(results.rows)
   })
@@ -21,7 +21,7 @@ const addMovies = (request, response) => {
 
   pool.query('INSERT INTO movies (name, description) VALUES ($1, $2)', [name, description], (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
     response.status(201).json({
       "message" : 'Insert Movie success !',
@@ -35,7 +35,7 @@ const addFestivals = (request, response) => {
 
   pool.query('INSERT INTO festivals (name, movie_id, start_date, end_date, description) VALUES ($1, $2, $3, $4, $5)', [name, movie_id, start_date, end_date, description], (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
     response.status(201).json({
       "message" : 'Insert Festival success !',
@@ -47,7 +47,7 @@ const addFestivals = (request, response) => {
 const getFestivals = (request, response) => {
   pool.query('Select * from festivals order by id ASC', (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
     response.status(200).json(results.rows)
   })
@@ -57,20 +57,83 @@ const getDetailMovie = (request, response) => {
   const id = parseInt(request.params.id)
   pool.query('Select * from movies where id = $1', [id], (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
     if (results.rows < 1 ){
       response.status(404).json({
         "message" : "Movie with id " + id + " is not found"
       })
+      return;
     }
     response.status(200).json(results.rows)
   })
 }
+
+const deleteMovie = (request, response) => {
+  const id = parseInt(request.params.id)
+  pool.query('Delete from movies where id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json({
+      "message": "Movie with id " + id + " is successfully deleted"
+    })
+  })
+}
+
+const deleteFestival = (request, response) => {
+  const id = parseInt(request.params.id)
+  pool.query('Delete from festivals where id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json({
+      "message": "Festival with id " + id + " is successfully deleted"
+    })
+  })
+}
+
+const getDetailFestival = (request, response) => {
+  const id = parseInt(request.params.id)
+  pool.query('Select * from festivals where id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    if (results.rows < 1 ){
+      response.status(404).json({
+        "message" : "Festival with id " + id + " is not found"
+      })
+      return;
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const searchFestival = (request, response) => {
+  const {name, start_date, end_date, movie_id} = request.body
+  pool.query('Select * from festivals where name like' + "'%"+ request.body.name + "%'" + " or movie_id = $1",[movie_id], (error, results) => {
+    if (error){
+      response.json(error)
+      return;
+    }
+    if (results.rows < 1 ){
+      response.status(404).json({
+        "message" : "Festival with id " + name + " is not found"
+      })
+      return;
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
 module.exports = {
   getMovies,
   addMovies,
   addFestivals,
   getFestivals,
-  getDetailMovie
+  getDetailMovie,
+  deleteMovie,
+  deleteFestival,
+  getDetailFestival,
+  searchFestival
 }
